7ac3e73c8deaec177c7f66d9977c1e321420eb80
BWIZA Annie <a.bwiza1@alustudent.com>
2025-06-04 03:22:05 -0700

    add mimi's archive script

--- hospital_data/archived_logs/archive_logs.sh
+++ hospital_data/archived_logs/archive_logs.sh
@@ -5,7 +5,8 @@ echo "1) heart_rate_log.log
 3) water_usage_log.log "
 read -p "Enter number: " choice
 #
-if [ $choice = "1" ];then
+case $choice in 
+	1)
 	original="/Gatchaman/hospital_data/active_logs/heart_rate_log.log"
 	timestamp=$(date -r "$original" +"%Y-%m-%d_%H:%M:%S")
 	new="heart_rate_${timestamp}.log"
@@ -13,7 +14,8 @@ if [ $choice = "1" ];then
 	echo "Archiving heart_rate.log..."
 	mv "$new" /Gatchaman/hospital_data/archived_logs/heart_data_archive/
 	echo "Successfully archived to heart_data_archive/$new"
-elif [ $choice = "2" ];then
+	;;
+       	2)
         original="/Gatchaman/hospital_data/active_logs/temperature_log.log"
         timestamp=$(date -r "$original" +"%Y-%m-%d_%H:%M:%S")
         new="temperature_${timestamp}.log"
@@ -21,8 +23,8 @@ elif [ $choice = "2" ];then
 	echo "Archiving temperature.log..."
         mv $new /Gatchaman/hospital_data/archived_logs/temperature_data_archive
 	echo "Successfully archived to temperature_data_archive/$new"
-
-elif [ $choice = "3" ];then
+	;;
+	3)
         original="/Gatchaman/hospital_data/active_logs/water_usage_log.log"
         timestamp=$(date -r "$original" +"%Y-%m-%d_%H:%M:%S")
         new="water_usage_${timestamp}.log"
@@ -30,12 +32,13 @@ elif [ $choice = "3" ];then
 	echo "Archiving water_usage.log..."
         mv $new /Gatchaman/hospital_data/archived_logs/water_usage_data_archive
 	echo "Successfully archived to water_usage_archive/$new"
-else
+	;;
+	*)
 	echo "your choice is incorrect try again btn (1-3)"
 	echo " Hello user, please choose a number below"
 	echo "  1) heart_rate_log.log
         	2) temperature_log.log
         	3) water_usage_log.log "
 	read -p "Enter number: " choice
-
-fi
+	;;
+esac
