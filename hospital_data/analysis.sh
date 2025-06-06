0992b7f19fbd601c3de036af794395d635306695
BWIZA Annie <a.bwiza1@alustudent.com>
2025-06-06 03:10:09 -0700

    u gon send this

--- hospital_data/analysis.sh
+++ hospital_data/analysis.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Ask the user to choose the file they want to work with
+#there is a while statement because we want the user to be prompted again to choose a number when they've chosen an invalid input
 while true; do
 echo "
 Select log file to analyze:
@@ -12,38 +13,46 @@ read -p "Enter choice (1-3): " user_input
 case "$user_input" in
 
 	1)	
-		#there was corrupted data in the heart_rate_log.log file where we had  device data with no device name and time,used set -x to debug every line 
-		awk 'NF>=3 {print $3}' /Gatchaman/hospital_data/active_logs/heart_rate_log.log | sort | uniq -c | sort -n
+		#there was corrupted data in the heart_rate_log.log file where we had  device data with no device name and time,used set -x to debug every line,had to add a condition of checking if there are 3 columns. 
+		device_count=$(awk 'NF>=3 {print $3}' /Gatchaman/hospital_data/active_logs/heart_rate_log.log | sort | uniq -c | sort -n)
+		echo "$device_count"
+		echo "$device_count" >> /Gatchaman/hospital_data/reports/analysis_report.txt
                 last_time=$(awk '{print $1," ",$2}' /Gatchaman/hospital_data/active_logs/heart_rate_log.log |sort -nr | head -n 1)
                 echo "the last time the heart  monitor recorded : $last_time"
                 first_time=$(awk '{print $1," ",$2}' /Gatchaman/hospital_data/active_logs/heart_rate_log.log | sort | head -n 1)
                 echo "the very first time the heart  monitor recorded: $first_time"
                 echo "the last time the heart monitor recorded was at $last_time" >> /Gatchaman/hospital_data/reports/analysis_report.txt
                 echo "the first time the heart monitor recorded was at  $first_time" >> /Gatchaman/hospital_data/reports/analysis_report.txt
-		echo -e "\n" >> /Gatchaman/hospital_data/reports/analysis_report.txt
-		break
-		;;
-
-	2)
-		awk '{print $3}' /Gatchaman/hospital_data/active_logs/temperature_log.log | sort | uniq -c | sort -n
-                last_time=$(awk '{print $1," ",$2}' /Gatchaman/hospital_data/active_logs/temperature_log.log |sort -nr | head -n 1)
-                echo "the last time the temperature monitor recorded : $last_time"
-                first_time=$(awk '{print $1," ",$2}' /Gatchaman/hospital_data/active_logs/temperature_log.log | sort | head -n 1)
-                echo "the very first time the temperature monitor recorded: $first_time"
-                echo "the last time the temperature monitor recorded was at $last_time" >> /Gatchaman/hospital_data/reports/analysis_report.txt
-                echo "the first time the temperature monitor recorded was at  $first_time" >> /Gatchaman/hospital_data/reports/analysis_report.txt
+		#added a new line after the report to avoid congestion of data
 		echo -e "\n" >> /Gatchaman/hospital_data/reports/analysis_report.txt
 		break
 		;;
 	
-	3)      awk '{print $3}' /Gatchaman/hospital_data/active_logs/water_usage_log.log | sort | uniq -c 
+
+	2)
+		device_count=$(awk '{print $3}' /Gatchaman/hospital_data/active_logs/temperature_log.log | sort | uniq -c | sort -n)
+		echo "$device_count"
+                echo "$device_count" >> /Gatchaman/hospital_data/reports/analysis_report.txt
+                last_time=$(awk '{print $1," ",$2}' /Gatchaman/hospital_data/active_logs/temperature_log.log |sort -nr | head -n 1)
+                echo "the last time the temperature monitor recorded : "$last_time""
+                first_time=$(awk '{print $1," ",$2}' /Gatchaman/hospital_data/active_logs/temperature_log.log | sort | head -n 1)
+                echo "the very first time the temperature monitor recorded: "$first_time""
+                echo "the last time the temperature monitor recorded was at "$last_time"" >> /Gatchaman/hospital_data/reports/analysis_report.txt
+		echo "the first time the temperature monitor recorded was at  "$first_time"" >> /Gatchaman/hospital_data/reports/analysis_report.txt
+		echo -e "\n" >> /Gatchaman/hospital_data/reports/analysis_report.txt
+		break
+		;;
+	
+	3)      device_count=$(awk '{print $3}' /Gatchaman/hospital_data/active_logs/water_usage_log.log | sort | uniq -c)
+		echo "$device_count"
+                echo "$device_count" >> /Gatchaman/hospital_data/reports/analysis_report.txt
                 last_time=$(awk '{print $1," ",$2}' /Gatchaman/hospital_data/active_logs/water_usage_log.log |sort -nr | head -n 1)
                 echo "the last time the water usage monitor recorded : $last_time"
                 first_time=$(awk '{print $1," ",$2}' /Gatchaman/hospital_data/active_logs/water_usage_log.log | sort | head -n 1)
                 echo "the very first time the water usage  monitor recorded: $first_time"
                 echo "the last time the water usage monitor recorded was at $last_time" >> /Gatchaman/hospital_data/reports/analysis_report.txt
                 echo "the first time the water usage monitor recorded was at  $first_time" >> /Gatchaman/hospital_data/reports/analysis_report.txt
-		echo -e "\n">> /Gatchaman/hospital_data/reports/analysis_report.txt
+		echo -e "\n" >> /Gatchaman/hospital_data/reports/analysis_report.txt
 		break
 		;;
 
@@ -52,3 +61,10 @@ case "$user_input" in
 		;;
 esac
 done
+#there is a break at the end of  each user_input choice  so that the user won't be prompted again and again
+#if the reports file doesn't exist
+if [ ! -f "/Gatchaman/hospital_data/reports/analysis_report.txt" ];then
+	echo " The report was not pushed to reports file"
+else 
+	echo "succesffully recorded in /reports/analysis_report.txt"
+fi
--- hospital_data/archive_logs.sh
+++ hospital_data/archive_logs.sh
@@ -1,44 +1,62 @@
 #!/bin/bash
-echo " Hello user, please choose a number below"
-echo "1) heart_rate_log.log
-2) temperature_log.log
-3) water_usage_log.log "
-read -p "Enter number: " choice
-#
-case $choice in 
-	1)
-	original="/Gatchaman/hospital_data/active_logs/heart_rate_log.log"
-	timestamp=$(date -r "$original" +"%Y-%m-%d_%H:%M:%S")
-	new="heart_rate_${timestamp}.log"
-	mv "$original" "$new"
-	echo "Archiving heart_rate.log..."
-	mv "$new" /Gatchaman/hospital_data/archived_logs/heart_data_archive/
-	echo "Successfully archived to heart_data_archive/$new"
-	;;
-       	2)
-        original="/Gatchaman/hospital_data/active_logs/temperature_log.log"
-        timestamp=$(date -r "$original" +"%Y-%m-%d_%H:%M:%S")
-        new="temperature_${timestamp}.log"
-	mv "$original" "$new"
-	echo "Archiving temperature.log..."
-        mv $new /Gatchaman/hospital_data/archived_logs/temperature_data_archive
-	echo "Successfully archived to temperature_data_archive/$new"
-	;;
-	3)
-        original="/Gatchaman/hospital_data/active_logs/water_usage_log.log"
-        timestamp=$(date -r "$original" +"%Y-%m-%d_%H:%M:%S")
-        new="water_usage_${timestamp}.log"
-	mv "$original" "$new"
-	echo "Archiving water_usage.log..."
-        mv $new /Gatchaman/hospital_data/archived_logs/water_usage_data_archive
-	echo "Successfully archived to water_usage_archive/$new"
-	;;
-	*)
-	echo "your choice is incorrect try again btn (1-3)"
+while true; do
 	echo " Hello user, please choose a number below"
-	echo "  1) heart_rate_log.log
-        	2) temperature_log.log
-        	3) water_usage_log.log "
+	echo "
+	1) heart_rate_log.log
+	2) temperature_log.log
+	3) water_usage_log.log "
 	read -p "Enter number: " choice
-	;;
-esac
+
+	case $choice in 
+		1)
+		original="/Gatchaman/hospital_data/active_logs/heart_rate_log.log"
+		timestamp=$(date -r "$original" +"%Y-%m-%d_%H:%M:%S")
+		new="heart_rate_${timestamp}.log"
+		mv "$original" "$new"
+		echo "Archiving heart_rate.log..."
+		dest_archive="/Gatchaman/hospital_data/archived_logs/heart_data_archive" 
+		mv "$new" "$dest_archive/"
+		echo "✔️Successfully archived to heart_data_archive/$new"
+		break
+		;;
+		2)
+       		original="/Gatchaman/hospital_data/active_logs/temperature_log.log"
+   		timestamp=$(date -r "$original" +"%Y-%m-%d_%H:%M:%S")
+       	 	new="temperature_${timestamp}.log"
+		mv "$original" "$new"
+		echo "Archiving temperature.log..."
+		dest_archive="/Gatchaman/hospital_data/archived_logs/temperature_data_archive"
+       		mv "$new" "$dest_archive/" 
+		echo "✔️Successfully archived to temperature_data_archive/$new"
+		break
+		;;
+		3)
+	        original="/Gatchaman/hospital_data/active_logs/water_usage_log.log"
+        	timestamp=$(date -r "$original" +"%Y-%m-%d_%H:%M:%S")
+	        new="water_usage_${timestamp}.log"
+		mv "$original" "$new"
+		echo "Archiving water_usage.log..."
+		dest_archive="/Gatchaman/hospital_data/archived_logs/water_usage_data_archive"
+	        mv "$new" "$dest_archive/"
+		echo "✔️Successfully archived to water_usage_archive/$new"
+		break
+		;;
+		*)
+		echo "your choice is incorrect try again btn (1-3)"
+		;;
+	esac
+done
+touch "$original"
+
+if [ ! -f "$original" ]; then
+	echo "oops!! Log file $original not found."
+    	exit 1
+fi
+if [ ! -d "$dest_archive" ]; then
+	echo "Archive directory "$dest_archive/" not found. Check again"
+fi
+if [ -f "$dest_archive/$new" ]; then
+    echo "Well done🎊🎊"
+else
+    echo "Failed to move $new to its destination archive"
+fi
